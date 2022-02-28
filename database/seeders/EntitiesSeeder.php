b2e833dd9865dc95ce8ff5d0ecd531c02d1f72ca
Takemitsu <takemitsu@notespace.jp>
2022-02-28 20:40:27 +0900

    update seeder

--- database/seeders/EntitiesSeeder.php
+++ database/seeders/EntitiesSeeder.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class EntitiesSeeder extends Seeder
 {
@@ -18,30 +19,37 @@ class EntitiesSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('days')->truncate();
+        // DB::table('days')->truncate();
+        // DB::table('entities')->truncate();
 
-        $user = User::where('name', 'takemitsu')
-            ->where('email', 'takemitsu@notespace.jp')
+        $user = User::where('name', 'nobody')
+            ->where('email', 'nobody@example.com')
             ->first();
-        if ($user) {
-            $now = Carbon::yesterday();
-            $now = $now->subDay();
-            for ($h = 1; $h < 4 + 1; $h++) {
-                $entities1 = Entity::firstOrCreate([
-                    'user_id' => $user->id,
-                    'name' => 'Test Group Name' . $h,
-                    'desc' => 'Test Group Desc' . $h,
-                ]);
+        if (!$user) {
+            $user = User::create([
+                'name' => 'nobody',
+                'email' => 'nobody@example.com',
+                'password' => Hash::make('nobodyTest123'),
+            ]);
+        }
 
-                for ($i = 0; $i < $h; $i++) {
-                    $now = $now->addDay();
-                    $day = Days::create([
-                        'entity_id' => $entities1->id,
-                        'name' => 'Test Entity ' . strval($i),
-                        'desc' => 'Text Entity Desc ' . $i,
-                        'anniv_at' => $now->format('Y-m-d'),
-                    ]);
-                }
+        $now = Carbon::yesterday();
+        $now = $now->subDay();
+        for ($h = 0; $h < 4 + 1; $h++) {
+            $entities1 = Entity::firstOrCreate([
+                'user_id' => $user->id,
+                'name' => 'Test Group Name' . $h,
+                'desc' => 'Test Group Desc' . $h,
+            ]);
+
+            for ($i = 0; $i < $h; $i++) {
+                $now = $now->addDay();
+                $day = Days::create([
+                    'entity_id' => $entities1->id,
+                    'name' => 'Test Entity ' . strval($i),
+                    'desc' => 'Text Entity Desc ' . $i,
+                    'anniv_at' => $now->format('Y-m-d'),
+                ]);
             }
         }
     }
